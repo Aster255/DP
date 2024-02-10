349dae3d489dcbfcceedce81cc5b61571915a991
Paul Adrian Alcos <paaalcos@gmail.com>
2024-02-11 02:40:18 +0800

    howSome

---
+++ howSum.js
@@ -0,0 +1,51 @@
+// Write a function `howSum(targetSum, numbers)' that takes in a
+// targetSum and an array of numbers as arguments.
+// The function should return an array containing any combination of
+// elements that add up to exactly the targetSum. If there is no
+// combination that adds up to the targetSum, then return null.=
+// If there are multiple combinations possible, you may return any
+// single one.
+
+const canSum = (n, arr, memo = {}) => {
+  if (n in memo) return memo[n];
+  if (n === 0) {
+    return true;
+  } else if (n < 0) {
+    return false;
+  } else {
+    for (e of arr) {
+      memo[e] = canSum(n - e, arr, memo);
+      if (memo[e] === true) {
+        return true;
+      }
+    }
+    memo[n] = false;
+    return false;
+  }
+};
+
+// console.log(canSum(7, [2, 3])); //true
+// console.log(canSum(7, [5, 3, 4, 7])); //true
+// console.log(canSum(7, [2, 4])); // false
+// console.log(canSum(8, [2, 3, 5])); // true
+// console.log(canSum(300, [7, 14])); // false
+
+const howSum = (n, arr, memo = {}) => {
+  const z = [];
+  if (!canSum(n, arr)) return null;
+  while (n) {
+    for (e of arr) {
+      if (canSum(n - e, arr, memo)) {
+        z.push(e);
+        n = n - e;
+      }
+    }
+  }
+  return z;
+};
+
+console.log(howSum(7, [2, 3]));
+console.log(howSum(7, [5, 3, 4, 7]));
+console.log(howSum(7, [2, 4]));
+console.log(howSum(8, [2, 3, 5]));
+console.log(howSum(300, [7, 14]));
